f7919fa4b6a7c89f107daff36d86c4c89b6eb98e
php_xh <1364052798@qq.com>
2019-05-27 17:54:44 +0800

    商品类目

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -2,6 +2,7 @@
 
 /* @var $factory \Illuminate\Database\Eloquent\Factory */
 
+use App\Models\Category;
 use App\Models\Product;
 use Faker\Generator as Faker;
 
@@ -18,6 +19,7 @@ $factory->define(Product::class, function (Faker $faker) {
         "https://iocaffcdn.phphub.org/uploads/images/201806/01/5320/2JMRaFwRpo.jpg",
         "https://iocaffcdn.phphub.org/uploads/images/201806/01/5320/pa7DrV43Mw.jpg",
     ]);
+    $category = Category::query()->where('is_directory', false)->inRandomOrder()->first();
 
     return [
         'title'        => $faker->word,
@@ -28,5 +30,8 @@ $factory->define(Product::class, function (Faker $faker) {
         'sold_count'   => 0,
         'review_count' => 0,
         'price'        => 0,
+        // 将取出的类目 ID 赋给 category_id 字段
+        // 如果数据库中没有类目则 $category 为 null，同样 category_id 也设成 null
+        'category_id'  => $category ? $category->id : null,
     ];
 });
