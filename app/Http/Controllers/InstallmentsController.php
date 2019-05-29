f4a9c5b7f5a2731167b487f155d4c4f6f9f52d99
php_xh <1364052798@qq.com>
2019-05-29 17:09:14 +0800

    商品

--- app/Http/Controllers/InstallmentsController.php
+++ app/Http/Controllers/InstallmentsController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers;
 use App\Events\OrderPaid;
 use App\Exceptions\InvalidRequestException;
 use App\Models\Installment;
+use App\Models\InstallmentItem;
+use App\Models\Order;
 use Carbon\Carbon;
+use Endroid\QrCode\QrCode;
 use Illuminate\Http\Request;
 
 class InstallmentsController extends Controller
@@ -126,5 +129,119 @@ class InstallmentsController extends Controller
 
         return app('alipay')->success();
     }
-    
+
+    public function payByWechat(Installment $installment)
+    {
+        if ($installment->order->closed) {
+            throw new InvalidRequestException('对应的商品订单已被关闭');
+        }
+        if ($installment->status === Installment::STATUS_FINISHED) {
+            throw new InvalidRequestException('该分期订单已结清');
+        }
+        if (!$nextItem = $installment->items()->whereNull('paid_at')->orderBy('sequence')->first()) {
+            throw new InvalidRequestException('该分期订单已结清');
+        }
+
+        $wechatOrder = app('wechat_pay')->scan([
+            'out_trade_no' => $installment->no.'_'.$nextItem->sequence,
+            'total_fee'    => $nextItem->total * 100,
+            'body'         => '支付 Laravel Shop 的分期订单：'.$installment->no,
+            'notify_url'   => ngrok_url('installments.wechat.notify'),
+        ]);
+        // 把要转换的字符串作为 QrCode 的构造函数参数
+        $qrCode = new QrCode($wechatOrder->code_url);
+
+        // 将生成的二维码图片数据以字符串形式输出，并带上相应的响应类型
+        return response($qrCode->writeString(), 200, ['Content-Type' => $qrCode->getContentType()]);
+    }
+
+    public function wechatNotify()
+    {
+        $data = app('wechat_pay')->verify();
+        if ($this->paid($data->out_trade_no, 'wechat', $data->transaction_id)) {
+            return app('wechat_pay')->success();
+        }
+
+        return 'fail';
+    }
+
+    protected function paid($outTradeNo, $paymentMethod, $paymentNo)
+    {
+        list($no, $sequence) = explode('_', $outTradeNo);
+        if (!$installment = Installment::where('no', $no)->first()) {
+            return false;
+        }
+        if (!$item = $installment->items()->where('sequence', $sequence)->first()) {
+            return false;
+        }
+        if ($item->paid_at) {
+            return true;
+        }
+
+        \DB::transaction(function () use ($paymentNo, $paymentMethod, $no, $installment, $item) {
+            $item->update([
+                'paid_at'        => Carbon::now(),
+                'payment_method' => $paymentMethod,
+                'payment_no'     => $paymentNo,
+            ]);
+
+            if ($item->sequence === 0) {
+                $installment->update(['status' => Installment::STATUS_REPAYING]);
+                $installment->order->update([
+                    'paid_at'        => Carbon::now(),
+                    'payment_method' => 'installment',
+                    'payment_no'     => $no,
+                ]);
+                event(new OrderPaid($installment->order));
+            }
+            if ($item->sequence === $installment->count - 1) {
+                $installment->update(['status' => Installment::STATUS_FINISHED]);
+            }
+        });
+
+        return true;
+    }
+
+    public function wechatRefundNotify(Request $request)
+    {
+        // 给微信的失败响应
+        $failXml = '<xml><return_code><![CDATA[FAIL]]></return_code><return_msg><![CDATA[FAIL]]></return_msg></xml>';
+        // 校验微信回调参数
+        $data = app('wechat_pay')->verify(null, true);
+        // 根据单号拆解出对应的商品退款单号及对应的还款计划序号
+        list($no, $sequence) = explode('_', $data['out_refund_no']);
+
+        $item = InstallmentItem::query()
+            ->whereHas('installment', function ($query) use ($no) {
+                $query->whereHas('order', function ($query) use ($no) {
+                    $query->where('refund_no', $no); // 根据订单表的退款流水号找到对应还款计划
+                });
+            })
+            ->where('sequence', $sequence)
+            ->first();
+
+        // 没有找到对应的订单，原则上不可能发生，保证代码健壮性
+        if (!$item) {
+            return $failXml;
+        }
+
+        // 如果退款成功
+        if ($data['refund_status'] === 'SUCCESS') {
+            // 将还款计划退款状态改成退款成功
+            $item->update([
+                'refund_status' => InstallmentItem::REFUND_STATUS_SUCCESS,
+            ]);
+
+            // 设定一个标志位
+            $item->installment->refreshRefundStatus();
+        } else {
+            // 否则将对应还款计划的退款状态改为退款失败
+            $item->update([
+                'refund_status' => InstallmentItem::REFUND_STATUS_FAILED,
+            ]);
+        }
+
+        return app('wechat_pay')->success();
+    }
+
 }
--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -109,6 +109,18 @@
               </li>
             </ul>
             <div class="tab-content">
+
+              <div class="properties-list">
+                <div class="properties-list-title">产品参数：</div>
+                <ul class="properties-list-body">
+{{--                  @foreach($product->properties as $property)--}}
+{{--                    <li>{{ $property->name }}：{{ $property->value }}</li>--}}
+{{--                  @endforeach--}}
+                  @foreach($product->grouped_properties as $name => $values)
+                    <li>{{ $name }}：{{ join(' ', $values) }}</li>
+                  @endforeach
+                </ul>
+              </div>
               <div role="tabpanel" class="tab-pane active" id="product-detail-tab">
                 {!! $product->description !!}
               </div>
