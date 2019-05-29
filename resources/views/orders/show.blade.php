6a57b389876d610772f6e58704f3f8db1ddaac04
php_xh <1364052798@qq.com>
2019-05-29 16:05:05 +0800

    前台分期付款详情页

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -257,6 +257,7 @@
           .then(function (response) {
             console.log(response.data);
             //  跳转到分期付款页面
+            location.href = '/installments/' + response.data.id;
           })
       });
 
